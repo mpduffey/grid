956585e190b5c1c09e2be812a193c55aedecf4f0
Mike Duffey <mpduffey@gmail.com>
2016-06-19 00:57:36 +0000

    v0.0.0

---
+++ grid.ts
@@ -0,0 +1,58 @@
+import {Component, Input, ChangeDetectionStrategy} from '@angular/core';
+
+@Component({
+	selector:					'grid',
+	inputs:						['rows: rows', 'columns: columns'],
+	templateUrl:			'app/components/grid/grid.html',
+	styles:						[`
+		table {
+			font-size: 12px;
+		}
+		thead td {
+			cursor: pointer;
+		}
+	`],
+	changeDetection:	ChangeDetectionStrategy.OnPush
+})
+
+export class Grid {
+    columns:Array<Column>;
+    rows:Array<any>;
+    @Input() name:string;
+    sorter = new Sorter();
+    sort(key){
+        this.sorter.sort(key, this.rows);
+    }
+}
+export class Column {
+	name: string;
+	descr: string;
+	constructor(name,descr){
+		this.name = name;
+		this.descr = descr;
+	}
+}
+export class Sorter{
+	direction:number;
+	key:string;
+	constructor(){
+		this.direction = 1;
+	}
+	sort(key,data){
+		if(this.key === key){
+			this.direction = this.direction * -1;
+		} else {
+			this.direction = 1;
+		}
+		this.key = key;
+		data.sort((a,b) => {
+			if(a[key] === b[key]){
+				return 0;
+			} else if(a[key] > b[key]) {
+				return 1 * this.direction;
+			} else {
+				return -1 * this.direction;
+			}
+		});
+	}
+}
